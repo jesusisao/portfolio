64887db1a3c33c5e061e5e44a761f08f297d9308
jesus_isao <kachintokita@yahoo.co.jp>
2019-08-25 00:39:11 +0900

    jsonをjsonifyしてファイルに書き出すスクリプトを追加

---
+++ tool/jsonToString.ts
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const consola = require("consola");
+
+// 引数0,1はts-nodeを使う都合上既に埋まっている
+if (process.argv.length !== 3) {
+  consola.error({
+    message: `引数にパスの指定が必要です。`,
+    badge: true
+  });
+  process.exit(1);
+}
+
+const jsonPath: string = process.argv[2];
+const userJson = require(jsonPath);
+const str = JSON.stringify(userJson);
+const distPath = "./tool/dist";
+try {
+  if (!fs.existsSync(distPath)) {
+    fs.mkdirSync(distPath);
+  }
+  fs.writeFileSync(`${distPath}/generated.txt`, str);
+  consola.success({
+    message: `completed.`,
+    badge: true
+  });
+} catch (e) {
+  console.log(e);
+}
